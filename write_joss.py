1f94c3e321a609a7f029d1ecaa4594f8a28c8bfe
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2020-11-01 13:44:08 -0800

    Create write_joss.py

---
+++ write_joss.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# read in lines from tex file
+f = open('main.tex', 'r')
+f1 = f.readlines()
+f.close()
+
+# assume scrape_joss.py has already been run
+num_reviewed = np.loadtxt('num_joss_reviews.txt')[0]
+
+if num_reviewed == 1:
+    project_word = 'project'
+else:
+    project_word = 'projects'
+    
+reviewed_line = f'\\item Reviewer, Journal of Open Source Software ({num_reviewed} {project_word} reviewed) (2020)\n'
+    
+for i, line in enumerate(f1):
+    if 'Reviewer, Journal of Open Source Software' in line:
+        f1[i] = reviewed_line
+        break
+        
+# write back to file
+f = open('main.tex', 'w')
+f.writelines(f1)
+f.close()
