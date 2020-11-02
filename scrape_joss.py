55ffb59cb2e989fbb9f3fbe1df9491e154ccd1c2
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2020-11-01 16:26:06 -0800

    Update scrape_joss.py

--- scrape_joss.py
+++ scrape_joss.py
@@ -15,7 +15,7 @@ print(os.listdir())
 
 
 sheets = Sheets.from_files('new_secret.json')
-os.remove(new_secret.json)
+os.remove('new_secret.json')
 
 joss_reviews_id = '1PAPRJ63yq9aPC1COLjaQp8mHmEq3rZUzwUYxTulyu78'
 
@@ -28,5 +28,5 @@ joss_table = pd.read_csv('test.csv')
 my_username = 'arjunsavel'
 
 review_col = 'Review count(last quarter)'
-num_reviews = joss_table[joss_table.username==my_username][review_col].values[0]
+num_reviews = int(joss_table[joss_table.username==my_username][review_col].values[0])
 np.savetxt('num_joss_reviews.txt', [num_reviews])
