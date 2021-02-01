a612bb23e1abaf7a089e0293a9a622030c8b17ba
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-01-31 20:43:35 -0800

    Update scrape_joss.py

--- scrape_joss.py
+++ scrape_joss.py
@@ -30,6 +30,6 @@ joss_table = pd.read_csv('test.csv')
 
 my_username = 'arjunsavel'
 
-review_col = 'Review count(last quarter)'
+review_col = 'Review count(all time)'
 num_reviews = int(joss_table[joss_table.username==my_username][review_col].values[0])
 np.savetxt('num_joss_reviews.txt', [num_reviews])
