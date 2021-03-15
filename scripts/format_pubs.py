40dde6bf41cb53b17e03585eb0363d706e9274f8
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-03-15 09:09:09 -0700

    for now, don't need npapers and nfirst!

--- scripts/format_pubs.py
+++ scripts/format_pubs.py
@@ -116,10 +116,14 @@ if __name__ == "__main__":
     ncitations = sum(cites)
     hindex = sum(c > i for i, c in enumerate(cites))
 
+#     summary = (
+#         "refereed: {1} / first author: {2} / citations: {3} / "
+#         "h-index: {4} ({0})"
+#     ).format(date.today(), npapers, nfirst, ncitations, hindex)
     summary = (
-        "refereed: {1} / first author: {2} / citations: {3} / "
-        "h-index: {4} ({0})"
-    ).format(date.today(), npapers, nfirst, ncitations, hindex)
+        "citations: {1} / "
+        "h-index: {2} ({0})"
+    ).format(date.today(), ncitations, hindex)
     with open("../supp_tex/pubs_summary.tex", "w") as f:
         f.write(summary)
 
