b5e1e2f7d029139f276828ee424ddb74d7ff84b3
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2022-12-06 00:01:28 -0500

    add JOSS map

--- scripts/format_pubs.py
+++ scripts/format_pubs.py
@@ -27,6 +27,7 @@ JOURNAL_MAP = {
     "IAU General Assembly": "IAU",
     "Astronomy and Astrophysics": "\\aanda",
     "American Astronomical Society Meeting Abstracts": "AAS",
+    "Journal of Open Source Software": "JOSS"
 }
 
 
