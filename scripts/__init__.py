3e7a0c6d3103b9e54939bf9b075a00b3e64a5c50
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2022-12-26 17:43:20 -0500

    Create __init__.py

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+__uri__ = "https://www.arjunsavel.com/PDFs/CV_Savel.pdf"
+__author__ = "Arjun B. Savel"
+__email__ = "asavel@gmail.com"
+__license__ = "MIT"
+__version__ = "0.1"
+__release__ = "0.1-beta"
+__description__ = "Automated CV."
