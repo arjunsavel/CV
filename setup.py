100f477e6f06365646c87b8f6fed35321580fe3d
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2022-12-26 17:42:23 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,85 @@
+# Inspired by:
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+###################################################################
+
+NAME = "CV"
+PACKAGES = find_packages(where="src")
+META_PATH = os.path.join("scripts", "__init__.py")
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+    "Programming Language :: Python",
+    'Topic :: Scientific/Engineering :: Astronomy'
+
+]
+INSTALL_REQUIRES = ["numpy",
+		   "matplotlib",
+		   "numba",
+		   "tqdm",
+		   "astropy",
+		   "pandas"]
+
+###################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=find_meta("uri"),
+        version=find_meta("version"),
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+	    package_data={'': ['README.md', 'LICENSE']},
+        long_description=read("README.md"),
+        long_description_content_type="text/x-rst",
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        options={"bdist_wheel": {"universal": "1"}},
+    )
